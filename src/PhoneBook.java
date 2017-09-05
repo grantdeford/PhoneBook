3844ae15aaf4cc61b439b4873304d6d022313af7
Grant DeFord <grantdeford@users.noreply.github.com>
2017-09-05 16:24:35 -0500

    adjustments--over halfway.

--- src/PhoneBook.java
+++ src/PhoneBook.java
@@ -1,13 +1,15 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class PhoneBook {
 	
-	//private ArrayList<Person> personalRecord = new ArrayList<Person>();
-	//private ArrayList<Address> personalAddress = new ArrayList<Address>();
-	
+	private ArrayList<Person> personalRecord = new ArrayList<Person>();
+	private ArrayList<Address> personalAddress = new ArrayList<Address>();
+	Scanner scanner = new Scanner(System.in);
 	public PhoneBook() {
-		Scanner scanner = new Scanner(System.in);
+		
 		String menuOptions;
 		
 		do {
@@ -33,7 +35,7 @@ public class PhoneBook {
 			
 		}while(menuOptions.equals("6") == false);
 		//scanner.close();
-		
+		scanner.nextLine();
 		///need to give choice to exit the program
 	}
 	
@@ -53,11 +55,99 @@ public class PhoneBook {
 	public void searchRecord() {
 		//method
 		//switch case for various options in search record or can pull from arrayList???
+		//Scanner scanner2 = new Scanner(System.in);
+		int option;
+		do {
+			System.out.println("How would you like to search for a record?");
+			System.out.println("1. Search for an existing record?");
+			System.out.println("2. Search a record by first name?");
+			System.out.println("3. Search a record by last name?");
+			System.out.println("4. Search a record by full name(first name, middle name, and last name?");
+			System.out.println("5. Search a record by telephone number?");
+			System.out.println("6. Search a record by city?");
+			System.out.println("7. Search a record by state?");
+			option = scanner.nextInt();
+				scanner.nextLine();
+				
+				switch(option) {
+				
+				case 1: 
+					System.out.println("Searching existing records.");
+					
+					//statements
+					//statements
+					break;
+				case 2:
+					System.out.println("Searching records by first name.");
+					//statements
+					
+					break;
+					
+				case 3: 
+					System.out.println("Searching records by last name.");
+					//statements
+					
+					break;
+				
+				case 4: 
+					System.out.println("Searching records for full name.");
+					//statements
+					
+					break;
+					
+				case 5:
+					System.out.println("Searching records by telephone number.");
+					
+					//statements
+					
+					break;
+					
+				case 6: 
+					System.out.println("Searching records by city.");
+					//statements
+					
+					break;
+					
+				case 7:
+					System.out.println("Searching records by state.");
+					//statements
+					
+					break;
+					
+					
+				default:
+					System.out.println("Invalid option, please try again");
+					
+				}
+				
+		}while(option.equals("8") == false;
 		
+		
+	
+			
+			
+	
 	}
 	public void showAllRecords() {
 		//method
 		//must be in ascending order--for loop
+		// A sample Java program to sort an array of strings
+		// in ascending and descending orders using Arrays.sort().
+		
+		 
+		public class SortExample
+		{
+		    public static void main(String[] args)
+		    {
+		        String arr[] = {"practice.geeksforgeeks.org",
+		                        "quiz.geeksforgeeks.org",
+		                        "code.geeksforgeeks.org"
+		                       };
+		 
+		        // Sorts arr[] in ascending order
+		        Arrays.sort(arr);
+		        System.out.printf("Modified arr[] : \n%s\n\n",
+		                          Arrays.toString(arr));
 	}
 	
 	public static void main(String[] args) {
